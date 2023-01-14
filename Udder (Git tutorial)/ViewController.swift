6ac07fadd553649333e417da2f756062c9217afb
Wahyu Utomo <why27bu@gmail.com>
2023-01-15 02:54:43 +0700

    Initial Commit

---
+++ Udder (Git tutorial)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder (Git tutorial)
+//
+//  Created by Wahyu Utomo on 15/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
